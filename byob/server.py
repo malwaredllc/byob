5a405271bb547361785bf9b1fdc4b7ce8e4baeea
Daniel Vega-Myhre <danielvegamyhre@gmail.com>
2018-08-01 16:37:08 -0600

    Minor bug fix

--- byob/server.py
+++ byob/server.py
@@ -90,7 +90,7 @@ def main():
         help='SQLite database')
 
     modules = os.path.abspath('modules')
-    packages = [os.path.abspath(_) for _ in sys.path if os.path.isdir(_) if os.path.basename(_) == 'site-packages'] if len([os.path.abspath(_) for _ in sys.path if os.path.isdir(_) if os.path.basename(_) == 'site-packages']) else [os.path.abspath(_) for _ in sys.path if os.path.isdir(_) if os.path.basename(_) == 'dist-packages']
+    packages = [os.path.abspath(_) for _ in sys.path if os.path.isdir(_) if os.path.basename(_) == 'site-packages'] if len([os.path.abspath(_) for _ in sys.path if os.path.isdir(_) if os.path.basename(_) == 'site-packages']) else [os.path.abspath(_) for _ in sys.path if os.path.isdir(_) if 'local' not in _ if os.path.basename(_) == 'dist-packages']
     
     if not len(packages):
         util.log("unable to locate 'site-packages' in sys.path (directory containing user-installed packages/modules)")
