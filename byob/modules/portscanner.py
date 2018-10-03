1107d17d9b94fe8239ec31675092fbda1fef7f86
malwaredllc <danielvegamyhre@gmail.com>
2018-10-03 08:46:46 -0600

    Bug fixes in portscanner module

--- byob/modules/portscanner.py
+++ byob/modules/portscanner.py
@@ -35,50 +35,55 @@ Scan a target IP/subnet for open ports and grab any service banners
 
 # main
 def _ping(host):
+    global results
     try:
-        if host not in globals()['results']:
-            if subprocess.call("ping -{} 1 -w 90 {}".format('n' if os.name == 'nt' else 'c', host), 0, None, subprocess.PIPE, subprocess.PIPE, subprocess.PIPE, shell=True) == 0:
-                globals()['results'][host] = {}
+        if host not in results:
+            if subprocess.call("ping -{} 1 -W 90 {}".format('n' if os.name == 'nt' else 'c', host), 0, None, subprocess.PIPE, subprocess.PIPE, subprocess.PIPE, shell=True) == 0:
+                results[host] = {}
                 return True
             else:
                 return False
         else:
             return True
-    except:
+    except Exception as e:
+        util.log(str(e))
         return False
 
 @util.threaded
 def _threader():
     while True:
+        global tasks
         try:
-            method, task = globals()['tasks'].get_nowait()
+            method, task = tasks.get_nowait()
             if callable(method):
                 _ = method(task)
-            globals()['tasks'].task_done()
+            tasks.task_done()
         except:
             break
 
 def _scan(target):
+    global ports
+    global results
     try:
+        host, port = target
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1.0)
-        sock.connect((str(target.host), int(target.port)))
+        sock.connect((str(host), int(port)))
         data = sock.recv(1024)
         sock.close()
         if data:
             data = ''.join([i for i in data if i in ([chr(n) for n in range(32, 123)])])
             data = data.splitlines()[0] if '\n' in data else str(data if len(str(data)) <= 80 else data[:77] + '...')
-            item = {str(target.port) : {'protocol': globals()['ports'][str(target.port)]['protocol'], 'service': data, 'state': 'open'}}
+            item = {str(port) : {'protocol': ports[str(port)]['protocol'], 'service': data, 'state': 'open'}}
         else:
-            item = {str(target.port) : {'protocol': globals()['ports'][str(target.port)]['protocol'], 'service': globals()['ports'][str(target.port)]['service'], 'state': 'open'}}
-        globals()['results'].get(target.host).update(item)
+            item = {str(port) : {'protocol': ports[str(port)]['protocol'], 'service': ports[str(port)]['service'], 'state': 'open'}}
+        results.get(host).update(item)
     except (socket.error, socket.timeout):
         pass
     except Exception as e:
-        print("{} error: {}".format(_scan.func_name, str(e)))
+        util.log("{} error: {}".format(_scan.func_name, str(e)))
 
-
-def run(target='192.168.1.1', subnet=False, ports=[21,22,23,25,80,110,111,135,139,443,445,993,995,1433,1434,3306,3389,8000,8008,8080,8888]):
+def run(target='192.168.1.1', ports=[21,22,23,25,80,110,111,135,139,443,445,554,993,995,1433,1434,3306,3389,8000,8008,8080,8888]):
     """ 
     Run a portscan against a target hostname/IP address
 
@@ -88,22 +93,21 @@ def run(target='192.168.1.1', subnet=False, ports=[21,22,23,25,80,110,111,135,13
     :returns: Results in a nested dictionary object in JSON format
 
     Returns onlne targets & open ports as key-value pairs in dictionary (JSON) object
+
     """
-#    try:
+    global tasks
+    global threads
+    global results
     if not util.ipv4(target):
         raise ValueError("target is not a valid IPv4 address")
-    print 'Scanning {} for open ports'.format(target)
-    globals()['targets'].append(target)
     if _ping(target):
         for port in ports:
-            globals()['tasks'].put_nowait((_scan, (target, port)))
-    if globals()['tasks'].qsize():
-        for i in range(1, int((globals()['tasks'].qsize() / 100) if globals()['tasks'].qsize() >= 100 else 1)):
+            tasks.put_nowait((_scan, (target, port)))
+        for i in range(1, tasks.qsize()):
             threads['portscan-%d' % i] = _threader()
-        if len(globals()['results']):
-            return dict({k: globals()['results'][k] for k in sorted(globals()['results'].keys()) if k in globals()['targets']})
-        else:
-            return "Target(s) offline"
-#    except Exception as e:
-#        print("{} error: {}".format(_scan.func_name, str(e)))
+        for t in threads:
+            threads[t].join()
+        return results[target]
+    else:
+        return "Target offline"
 
