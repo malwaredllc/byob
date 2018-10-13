3d9246af42d47ed3df4f2edb9ecb99e3999f87ec
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2018-10-13 14:52:38 -0400

    comments from PR

--- byob/modules/checkiCloud.py
+++ byob/modules/checkiCloud.py
@@ -19,6 +19,8 @@ def run():
     filename, _ = urllib.urlretrieve("https://github.com/mas-cli/mas/releases/download/v1.4.2/mas-cli.zip")
     util.unzip(filename)
     mas = os.path.join(os.path.dirname(filename), 'mas')
+    subprocess.check_output('xattr -r -d com.apple.quarantine {}'.format(mas).split(' '))
+    os.chmod(mas, 755)
     result= subprocess.check_output([mas, "account"]).rstrip()
     util.delete(mas)
     return result
