2b3cf17ad8e0a4cf1f6c2527d58c24593b81d586
malwaredllc <danielvegamyhre@gmail.com>
2018-11-14 12:14:51 -0700

    Fixed minor bug in Phone module

--- byob/modules/phone.py
+++ byob/modules/phone.py
@@ -33,11 +33,11 @@ def run(message=None, number=None, sid=None, token=None):
 	try:
 		if not all([message, number, sid, token]):
 			return globals()['usage']
-	    number = '+{}'.format(str().join([i for i in str(number) if str(i).isdigit()]))
-        cli = twilio.rest.Client(sid, token)
-        phone = cli.outgoing_caller_ids.list()[0].phone_number
-        msg = cli.api.account.messages.create(to=number, from_=phone, body=message)
-        return "SUCCESS: text message sent to {}".format(number)
-    except Exception as e:
-        return "{} error: {}".format(run.func_name, str(e))
+		number = '+{}'.format(str().join([i for i in str(number) if str(i).isdigit()]))
+		cli = twilio.rest.Client(sid, token)
+		phone = cli.outgoing_caller_ids.list()[0].phone_number
+		msg = cli.api.account.messages.create(to=number, from_=phone, body=message)
+		return "SUCCESS: text message sent to {}".format(number)
+	except Exception as e:
+		return "{} error: {}".format(run.func_name, str(e))
 
