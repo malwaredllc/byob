ede685b78fd205a746134bb95c289d43f2ba2629
malwaredllc <danielvegamyhre@gmail.com>
2020-04-14 13:45:29 -0600

    Add hidden import pkg_resources.py2_warn due to setuptools issue #1963

--- byob/core/generators.py
+++ byob/core/generators.py
@@ -236,7 +236,7 @@ def freeze(filename, icon=None, hidden=None):
     # fspec = os.path.join(path, name + '.spec')
     # with open(fspec, 'w') as fp:
     #     fp.write(spec)
-    process = subprocess.Popen('{0} -m PyInstaller --onefile {1}'.format(sys.executable, filename), 0, None, subprocess.PIPE, subprocess.PIPE, subprocess.PIPE, shell=True)
+    process = subprocess.Popen('{0} -m PyInstaller --onefile --hidden-import="pkg_resources.py2_warn" {1}'.format(sys.executable, filename), 0, None, subprocess.PIPE, subprocess.PIPE, subprocess.PIPE, shell=True)
     while True:
         try:
             line = process.stderr.readline().rstrip()
