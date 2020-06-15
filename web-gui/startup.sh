7f1d464e21b6eaf226c0c7076d413157a508678b
dontbanmeplz <59851616+dontbanmeplz@users.noreply.github.com>
2020-06-15 16:28:48 -0400

    Update startup.sh

--- web-gui/startup.sh
+++ web-gui/startup.sh
@@ -32,7 +32,7 @@ python3 -m pip install -r requirements.txt
 
 # Build Docker images
 echo "Building Docker images - this will take a while, please be patient..."
-cd docker-pyinstaller
+cd docker-pyinstaller1
 docker build -f Dockerfile-py3-amd64 -t nix-amd64 .
 docker build -f Dockerfile-py3-i386 -t nix-i386 .
 docker build -f Dockerfile-py3-win32 -t win-win32 .
