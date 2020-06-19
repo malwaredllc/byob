e8714cb33332752a77a769c73c6f04e140322847
malwaredllc <danielvegamyhre@gmail.com>
2020-06-18 19:43:34 -0600

    hotfix in startup.sh misnaming winx32 docker image

--- web-gui/startup.sh
+++ web-gui/startup.sh
@@ -35,7 +35,7 @@ echo "Building Docker images - this will take a while, please be patient..."
 cd docker-pyinstaller1
 docker build -f Dockerfile-py3-amd64 -t nix-amd64 .
 docker build -f Dockerfile-py3-i386 -t nix-i386 .
-docker build -f Dockerfile-py3-win32 -t win-win32 .
+docker build -f Dockerfile-py3-win32 -t win-x32 .
 
 # Run app
 cd ..
