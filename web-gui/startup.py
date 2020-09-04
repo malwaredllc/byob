b0e6a6ea27237dd4a9e3820f0d13e5ae1e3877a9
dontbanmeplz <59851616+dontbanmeplz@users.noreply.github.com>
2020-09-04 10:47:08 -0500

    Update startup.py

--- web-gui/startup.py
+++ web-gui/startup.py
@@ -1,5 +1,4 @@
 import os
-import urllib.request
 import time
 
 print("Running BYOB app startup...")
@@ -9,16 +8,14 @@ status = os.system("which python3")
 
 if status == "":
 	print("Installing Python 3.6...")
-  print("In 5 secconds there will be a popup asking for you to put in an admin password. This is the python install exe running. This is NOT a virus the hashes for this file are in 'hashes.txt'.")
+  	print("In 5 secconds there will be a popup asking for you to put in an admin password. This is the python install exe running. This is NOT a virus the hashes for this file are in 'hashes.txt'.")
 	time.sleep(5)
-  os.system("python-3.8.5-amd64.exe")
-else
-	echo "Confirmed Python is installed."
-fi
+  	os.system("python-3.8.5-amd64.exe")
+else:
+	print("Confirmed Python is installed.")
 
 # Install Docker if necessary
-which docker > /dev/null
-status=$?
+status2 = os.system("which docker")
 
 if test $status -ne 0
 then
@@ -29,19 +26,15 @@ else
 fi
 
 # Install Python packages
-echo "Installing Python packages..."
-python3 -m pip install -r requirements.txt
-apt-get install snapd -y
-snap install ngrok
+print("Installing Python packages...")
+os.system("python3 -m pip install -r requirements.txt")
 # Build Docker images
-echo "Building Docker images - this will take a while, please be patient..."
-cd docker-pyinstaller1
-docker build -f Dockerfile-py3-amd64 -t nix-amd64 .
-docker build -f Dockerfile-py3-i386 -t nix-i386 .
-docker build -f Dockerfile-py3-win32 -t win-x32 .
+os.system("Building Docker images - this will take a while, please be patient...")
+os.system("cd docker-pyinstaller1")
+os.system("docker build -f Dockerfile-py3-amd64 -t nix-amd64 .")
+os.system("docker build -f Dockerfile-py3-i386 -t nix-i386 .")
+os.system("docker build -f Dockerfile-py3-win32 -t win-x32 .")
 
 # Run app
-cd ..
-echo "Running C2 server with locally hosted web app GUI..."
-echo "Navigate to http://127.0.0.1:5000 and set up your user to get started."
-python3 run.py
+os.system("cd ..")
+os.system("python3 run.py")
