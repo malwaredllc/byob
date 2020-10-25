8e210678cb2fbda9c59dedb92a1bbd41911f9e08
vrlnx <vrlinux@pm.me>
2020-10-25 23:46:03 +0100

    update- because of major error, idk

--- web-gui/docker-pyinstaller1/entrypoint-windows.sh
+++ web-gui/docker-pyinstaller1/entrypoint-windows.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Make sure .bashrc is sourced
-. /root/.bashrc
+. ~/.bashrc
 
 # Allow the workdir to be set using an env var.
 # Useful for CI pipiles which use docker for their build steps
@@ -15,26 +15,24 @@ WORKDIR=${SRCDIR:-/src}
 # In case the user specified a custom URL for PYPI, then use
 # that one, instead of the default one.
 #
-
 if [[ "$PYPI_URL" != "https://pypi.python.org/" ]] || \
    [[ "$PYPI_INDEX_URL" != "https://pypi.python.org/simple" ]]; then
     # the funky looking regexp just extracts the hostname, excluding port
     # to be used as a trusted-host.
-    mkdir -p /wine/drive_c/users/root/pip
-    echo "[global]" > /wine/drive_c/users/root/pip/pip.ini
-    echo "index = $PYPI_URL" >> /wine/drive_c/users/root/pip/pip.ini
-    echo "index-url = $PYPI_INDEX_URL" >> /wine/drive_c/users/root/pip/pip.ini
-    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /wine/drive_c/users/root/pip/pip.ini
+    mkdir -p /wine/drive_c/users~/pip
+    echo "[global]" > /wine/drive_c/users~/pip/pip.ini
+    echo "index = $PYPI_URL" >> /wine/drive_c/users~/pip/pip.ini
+    echo "index-url = $PYPI_INDEX_URL" >> /wine/drive_c/users~/pip/pip.ini
+    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /wine/drive_c/users~/pip/pip.ini
 
     echo "Using custom pip.ini: "
-    cat /wine/drive_c/users/root/pip/pip.ini
-    
+    cat /wine/drive_c/users~/pip/pip.ini
 fi
 
 cd $WORKDIR
 
 if [ -f requirements.txt ]; then
-    python3 -m pip install -r requirements.txt
+    pip install -r requirements.txt
 fi # [ -f requirements.txt ]
 
 echo "$@"
@@ -45,4 +43,3 @@ if [[ "$@" == "" ]]; then
 else
     sh -c "$@"
 fi # [[ "$@" == "" ]]
-
