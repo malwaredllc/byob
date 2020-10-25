1962d78cc2969816a69e09f54e374d907d15f689
vrlnx <vrlinux@pm.me>
2020-10-25 02:33:28 +0100

    update

--- web-gui/startup.sh
+++ web-gui/startup.sh
@@ -3,39 +3,25 @@
 echo "Running BYOB app startup..."
 
 # Install Python if necessary
-which python3 > /dev/null
-status=$?
 
-if test $status -ne 0
-then
-	echo "Installing Python 3.6..."
-	sudo apt install python3.6 -y
-else
-	echo "Confirmed Python is installed."
-fi
+which python3 > /dev/null ; status=$! ; [ ! $(test) $status 0 ] && echo "Installing Python 3.6..." ; sudo apt install -y python3.6 || echo "Confirmed Python3 is installed."
 
 # Install Docker if necessary
-which docker > /dev/null
-status=$?
 
-if test $status -ne 0
-then
-	echo "Installing Docker..."
-	apt install docker.io -y
-else
-	echo "Confirmed Docker is installed."
-fi
+which docker > /dev/null ; status=$! ; [ ! $(test) $status 0 ] && echo "Installing Docker..." ; sudo apt install docker.io -y || echo "Confirmed Docker is installed."
+
 
 # Install Python packages
 echo "Installing Python packages..."
-pip3 install -r requirements.txt
+cd ~/byob/web-gui/
+python3 -m pip install -r requirements.txt
 
 # Build Docker images
 echo "Building Docker images - this will take a while, please be patient..."
-cd docker-pyinstaller1
-docker build -f Dockerfile-py3-amd64 -t nix-amd64 .
-docker build -f Dockerfile-py3-i386 -t nix-i386 .
-docker build -f Dockerfile-py3-win32 -t win-x32 .
+cd docker-pyinstaller1 \
+; docker build -f Dockerfile-py3-amd64 -t nix-amd64 . \
+; docker build -f Dockerfile-py3-i386 -t nix-i386 . \
+; docker build -f Dockerfile-py3-win32 -t win-x32 .
 
 # Run app
 cd ..
