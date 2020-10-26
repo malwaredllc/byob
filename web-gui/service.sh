726ffdeab851949c5bb2083fbe05e6c3ba0c1725
vrlnx <vrlinux@pm.me>
2020-10-26 04:14:38 +0100

    update

--- web-gui/service.sh
+++ web-gui/service.sh
@@ -2,10 +2,10 @@
 # Build Docker images
 echo "Building Docker images - this will take a while, please be patient..."
 echo ""
-cd ~/byob/web-gui/docker-pyinstaller1 \
-; docker build -f Dockerfile-py3-amd64 -t nix-amd64 . >& /dev/null \
-; docker build -f Dockerfile-py3-i386 -t nix-i386 . >& /dev/null \
-; docker build -f Dockerfile-py3-win32 -t win-x32 . >& /dev/null \
+cd $HOME/byob/web-gui/docker-pyinstaller1 \
+; docker build -f Dockerfile-py3-amd64 -t nix-amd64 . \
+; docker build -f Dockerfile-py3-i386 -t nix-i386 .  \
+; docker build -f Dockerfile-py3-win32 -t win-x32 .
 # Run app
-; cd ~/byob/web-gui/
+cd $HOME/byob/web-gui/
 python3 run.py
