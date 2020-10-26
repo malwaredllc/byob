ed857dbb6daf6ae7238da9acf2ab73e460f1066f
vrlnx <vrlinux@pm.me>
2020-10-26 03:12:21 +0100

    update

--- web-gui/startup.sh
+++ web-gui/startup.sh
@@ -17,8 +17,6 @@ SPIN_PID=$!
 # Build Docker images
 echo "Building Docker images - this will take a while, please be patient..."
 echo ""
-echo ""
-echo ""
 echo "Please wait about 15-30 min before the server is started up..."
 echo "On slower PC's it can be as long as up to 1-2 hours..."
 echo ""
@@ -26,7 +24,7 @@ cd docker-pyinstaller1 \
 ; docker build -f Dockerfile-py3-amd64 -t nix-amd64 . >& /dev/null \
 ; docker build -f Dockerfile-py3-i386 -t nix-i386 . >& /dev/null \
 ; docker build -f Dockerfile-py3-win32 -t win-x32 . >& /dev/null \
-; kill -9 $SPIN_PID >& /dev/null
+; kill -9 $SPIN_PID
 sleep .05
 echo ""
 echo ""
