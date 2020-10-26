2b21ff070d982c1e619559f9b194a409775f18f5
vrlnx <vrlinux@pm.me>
2020-10-26 02:25:49 +0100

    update

--- web-gui/startup.sh
+++ web-gui/startup.sh
@@ -1,28 +1,41 @@
 #/bin/sh
-
+spin()
+{
+spinner="/|\\-/|\\-"
+while :
+do
+    for i in `seq 0 7`
+    do
+    echo -n "${spinner:$i:1}"
+    echo -en "\010"
+    sleep .15
+    done
+done
+}
 echo "Running BYOB app startup..."
-
+spin & SPIN_PID=$!
 # Install Python if necessary
 
-which python3 > /dev/null ; status=$! ; [ ! $(test) $status 0 ] && echo "Installing Python 3.6..." ; sudo apt install -y python3.6 || echo "Confirmed Python3 is installed."
+which python3 >& /dev/null ; status=$! ; [ ! $(test) $status 0 ] && echo "Installing Python 3.6..." ; sudo apt install -y python3.6 || echo "Confirmed Python3 is installed."
 
 # Install Docker if necessary
 
-which docker > /dev/null ; status=$! ; [ ! $(test) $status 0 ] && echo "Installing Docker..." ; sudo apt install docker.io -y || echo "Confirmed Docker is installed."
+which docker >& /dev/null ; status=$! ; [ ! $(test) $status 0 ] && echo "Installing Docker..." ; sudo apt install docker.io -y || echo "Confirmed Docker is installed."
 
 
 # Install Python packages
 echo "Installing Python packages..."
 cd ~/byob/web-gui/
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt >& /dev/null
 
 # Build Docker images
 echo "Building Docker images - this will take a while, please be patient..."
 cd docker-pyinstaller1 \
-; docker build -f Dockerfile-py3-amd64 -t nix-amd64 . \
-; docker build -f Dockerfile-py3-i386 -t nix-i386 . \
-; docker build -f Dockerfile-py3-win32 -t win-x32 .
-
+; docker build -f Dockerfile-py3-amd64 -t nix-amd64 . >& /dev/null \
+; docker build -f Dockerfile-py3-i386 -t nix-i386 . >& /dev/null \
+; docker build -f Dockerfile-py3-win32 -t win-x32 . >& /dev/null
+kill -9 $SPIN_PID >& /dev/null
+sleep .05
 # Run app
 cd ..
 echo "Running C2 server with locally hosted web app GUI..."
