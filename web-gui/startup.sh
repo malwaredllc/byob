f611b7ec7539dc3a08ad87a57d38bf78b4d96f6d
vrlnx <vrlinux@pm.me>
2020-10-26 03:09:15 +0100

    update

--- web-gui/startup.sh
+++ web-gui/startup.sh
@@ -16,23 +16,27 @@ spin &
 SPIN_PID=$!
 # Build Docker images
 echo "Building Docker images - this will take a while, please be patient..."
+echo ""
+echo ""
+echo ""
 echo "Please wait about 15-30 min before the server is started up..."
 echo "On slower PC's it can be as long as up to 1-2 hours..."
+echo ""
 cd docker-pyinstaller1 \
 ; docker build -f Dockerfile-py3-amd64 -t nix-amd64 . >& /dev/null \
 ; docker build -f Dockerfile-py3-i386 -t nix-i386 . >& /dev/null \
 ; docker build -f Dockerfile-py3-win32 -t win-x32 . >& /dev/null \
 ; kill -9 $SPIN_PID >& /dev/null
 sleep .05
+echo ""
+echo ""
 # Run app
 cd ..
 echo "Don't use 0.0.0.0:5000 - That is just a testing IP."
 echo "Due to the ever developing of BYOB, 0.0.0.0:5000 stays for now"
 echo ""
 echo ""
-echo ""
 echo "Open URL in browser: ""$HOSTNAME"".local:5000"
-echo ""
 echo ""
 echo ""
 python3 run.py
