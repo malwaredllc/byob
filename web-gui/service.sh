b60a9116ca994e6027fba1781edf3db9f2b05344
vrlnx <vrlinux@pm.me>
2020-10-26 04:12:38 +0100

    update

--- web-gui/service.sh
+++ web-gui/service.sh
@@ -7,5 +7,5 @@ cd ~/byob/web-gui/docker-pyinstaller1 \
 ; docker build -f Dockerfile-py3-i386 -t nix-i386 . >& /dev/null \
 ; docker build -f Dockerfile-py3-win32 -t win-x32 . >& /dev/null \
 # Run app
-cd ~/byob/web-gui/
+; cd ~/byob/web-gui/
 python3 run.py
