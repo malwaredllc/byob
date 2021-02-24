a39e68f8c3d3696e2d67ca42be34b74fc8ccc9c8
malwaredllc <danielvegamyhre@gmail.com>
2021-02-23 22:41:59 -0700

    work on unit tests

--- web-gui/tests/unit/test_models.py
+++ web-gui/tests/unit/test_models.py
@@ -1,10 +1,13 @@
 
 import pytest
-from buildyourownbotnet.models import User
+from buildyourownbotnet import app, db, bcrypt
+from buildyourownbotnet.models import User, Session, Payload
 
 @pytest.fixture(scope='module')
 def new_user():
-    user = User(username='test_user', password='test_password')
+    test_username = 'test_user'
+    hashed_password = bcrypt.generate_password_hash('test_password').decode('utf-8')
+    user = User(username=test_username, password=hashed_password)
     return user
 
 def test_new_user_with_fixture(new_user):
@@ -15,7 +18,6 @@ def test_new_user_with_fixture(new_user):
     """
     assert new_user.username == 'test_user'
     assert new_user.password != 'test_password'
-
 
 def test_new_session():
     """
