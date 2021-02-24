7341e396d62634302a6f08692e91bcb744979ed6
malwaredllc <danielvegamyhre@gmail.com>
2021-02-24 09:46:15 -0700

    Fixing bug related to docker-pyinstaller

--- web-gui/startup.sh
+++ web-gui/startup.sh
@@ -51,7 +51,7 @@ python3 -m pip install -r requirements.txt
 
 # Build Docker images
 echo "Building Docker images - this will take a while, please be patient..."
-cd docker-pyinstaller1
+cd docker-pyinstaller
 docker build -f Dockerfile-py3-amd64 -t nix-amd64 .
 docker build -f Dockerfile-py3-i386 -t nix-i386 .
 docker build -f Dockerfile-py3-win32 -t win-x32 .
