8d1f8e8f94560385bccae1e542a1608e6cb0d084
malwaredllc <danielvegamyhre@gmail.com>
2021-03-01 22:04:18 -0700

    test dao handle session with no uid

--- web-gui/tests/functional/test_dao.py
+++ web-gui/tests/functional/test_dao.py
@@ -13,7 +13,55 @@ def test_handle_session(new_user):
     when a new user is created via dao.handle_session function,
     then check the session metadata is stored in the database correctly. 
     """
-    # add test session
+    # add test session (without uid)
+    uid = md5(bytes(getrandbits(10))).hexdigest()
+    input_session_dict = {
+			"online": True,
+			"joined": datetime.utcnow(),
+			"last_online": datetime.utcnow(),
+			"public_ip": '1.2.3.4',
+			"local_ip": '192.1.1.168',
+			"mac_address": '00:0A:95:9D:68:16',
+			"username": 'test_user',
+			"administrator": True,
+			"platform": 'linux2',
+			"device": 'test_device',
+			"architecture": 'x32',
+			"latitude": 0.00,
+			"longitude": 0.00,
+			"owner": new_user.username,
+    }
+    output_session_dict = dao.handle_session(input_session_dict)
+
+    # run tests
+    assert 'uid' in output_session_dict
+    uid = output_session_dict['uid']
+
+    session_query = Session.query.filter_by(uid=uid)
+    assert len(session_query.all()) == 1
+
+    session = session_query.first()
+    assert session.owner == new_user.username
+    assert session.uid == uid
+    assert session.online is True
+    assert (datetime.utcnow() - session.joined).seconds <= 5
+    assert (datetime.utcnow() - session.last_online).seconds <= 5
+    assert session.public_ip == '1.2.3.4'
+    assert session.local_ip == '192.1.1.168'
+    assert session.mac_address == '00:0A:95:9D:68:16'
+    assert session.username == 'test_user'
+    assert session.administrator is True
+    assert session.platform == 'linux2'
+    assert session.device == 'test_device'
+    assert session.architecture == 'x32'
+    assert session.longitude == 0.00
+    assert session.latitude == 0.00
+
+    # clean up
+    session_query.delete()
+    db.session.commit()
+
+    # add test session (with uid)
     uid = md5(bytes(getrandbits(10))).hexdigest()
     input_session_dict = {
 			"uid": uid,
