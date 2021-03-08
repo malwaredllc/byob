741c46720e716205e11af0e9136c6fd4b6384a0f
malwaredllc <danielvegamyhre@gmail.com>
2021-03-07 17:17:19 -0700

    finish login/logout auth tests

--- web-gui/tests/unit/test_routes.py
+++ web-gui/tests/unit/test_routes.py
@@ -1,4 +1,5 @@
 import pytest
+from flask_login import current_user
 from ..conftest import app_client, new_user
 
 def login(client, username, password):
@@ -22,6 +23,7 @@ def test_login_valid(app_client, new_user):
     """
     response = login(app_client, 'test_user', 'test_password')
     assert response.status_code == 200
+    assert new_user.is_authenticated is True    
 
 def test_login_invalid(app_client, new_user):
     """
@@ -31,5 +33,23 @@ def test_login_invalid(app_client, new_user):
     """
     response = login(app_client, 'test_user', 'wrong_password')
     assert response.status_code == 403
+    assert current_user.is_authenticated is False
 
+def test_logout(app_client, new_user):
+    """
+    Given a logged in user,
+    when that user sends a GET request to /logout,
+    check that the user is no longer authenticated.
+    """
+    login(app_client, new_user.username, 'test_password')
+    assert current_user.is_authenticated is True
+    logout(app_client)
+    assert current_user.is_authenticated is False
 
+def test_sessions_authenticated(app_client, new_user):
+    """
+    Given a logged in user,
+    when that user sends a GET request to /account,
+    check that a HTTP 200 response is returned.
+    """
+    pass
