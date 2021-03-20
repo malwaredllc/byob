d35568bf398172e35ffa4a70e979a3e4ce03a11a
malwaredllc <30509968+malwaredllc@users.noreply.github.com>
2021-03-20 16:42:02 -0600

    update test with timestamp checking

--- web-gui/tests/conftest.py
+++ web-gui/tests/conftest.py
@@ -1,9 +1,13 @@
+import os
 import pytest
+import shutil
 from hashlib import md5
 from random import getrandbits
 from datetime import datetime
 from buildyourownbotnet import create_app
 from buildyourownbotnet.models import db, bcrypt, User, Payload, Session, Task, ExfiltratedFile
+
+OUTPUT_DIR = os.path.abspath('buildyourownbotnet/output')
 
 @pytest.fixture(scope='function')
 def app_client():
@@ -17,11 +21,30 @@ def app_client():
 def new_user():
 	test_username = 'test_user'
 	user = User.query.filter_by(username=test_username).first()
+
+	# create user in database
 	if not user:
 		hashed_password = bcrypt.generate_password_hash('test_password').decode('utf-8')
 		user = User(username=test_username, password=hashed_password)
 		db.session.add(user)
 		db.session.commit()
+
+	# create user directory
+	user_dir = os.path.join(OUTPUT_DIR, user.username)
+	if not os.path.exists(user_dir):
+		os.makedirs(user_dir)
+
+	# create user src directory
+	src_dir = os.path.join(user_dir, 'src')
+	if not os.path.exists(src_dir):
+		os.makedirs(src_dir)
+
+	# create user exfiltrated files directory
+	files_dir = os.path.join(user_dir, 'files')
+	if not os.path.exists(files_dir):
+		os.makedirs(files_dir)
+
+	print(src_dir)
 	yield user
 	cleanup()
 
@@ -53,14 +76,16 @@ def new_session(new_user):
 	cleanup()
 
 def cleanup():
-    """
-    Helper function for cleaning up database after tests.
-    """
-    User.query.delete()
-    Session.query.delete()
-    Task.query.delete()
-    ExfiltratedFile.query.delete()
-    db.session.commit()
+	"""
+	Helper function for cleaning up database after tests.
+	"""
+	# clean up database
+	User.query.delete()
+	Session.query.delete()
+	Task.query.delete()
+	ExfiltratedFile.query.delete()
+	db.session.commit()
+
 
 def login(client, username, password):
     return client.post('/login', 
---
+++ web-gui/tests/unit/test_payload_api.py
@@ -0,0 +1,41 @@
+import os
+import pytest
+import shutil
+
+from hashlib import md5
+from datetime import datetime
+from random import getrandbits
+from buildyourownbotnet.server import SessionThread
+from buildyourownbotnet.models import Session
+from ..conftest import app_client, new_user, login, cleanup
+
+
+def test_api_payload_generate(app_client, new_user):
+    """
+    Given an authenticated user,
+    when a POST request is sent to /api/payload/generate endpoint with valid parameters,
+    check that the payload is generated correctly and metadata is stored in the database correctly.
+    """
+    login(app_client, new_user.username, 'test_password')
+    res = app_client.post('/api/payload/generate', 
+            data={'format': 'py'},
+            follow_redirects=True, 
+            headers = {"Content-Type":"application/x-www-form-urlencoded"}
+    )
+    # if successful, user will be redirected back to payload generator where they will see a success message
+    #assert res.status_code == 302 
+    user_dir = os.path.join('./buildyourownbotnet/output/', new_user.username)
+    src_dir = os.path.join(user_dir, 'src')
+    user_files = os.listdir(src_dir)
+
+    # check if a new payload file has been created in the last 5 seconds
+    for f in user_files:
+        fpath = os.path.join(src_dir, f)
+        ctime = datetime.fromtimestamp(os.stat(fpath).st_ctime)
+        if (datetime.now() - ctime).seconds <= 10:
+            break
+    else:
+        pytest.fail(f"No recently created payload found in {src_dir}")
+
+    # clean up filesystem
+    shutil.rmtree(user_dir)
