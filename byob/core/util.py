e7b02f442d71b92be5b7815188b974acdfdd5246
winterrdog <87463258+winterrdog@users.noreply.github.com>
2021-08-16 20:13:19 +0300

    ctypes library updated
    The previous call wasn't working because the ctypes API was updated to use a WinDLL() class. i have tested this with Python3.9 and it worked successfully. Ref: https://docs.python.org/3/library/ctypes.html?highlight=ctypes#ctypes.WinDLL

--- byob/core/util.py
+++ byob/core/util.py
@@ -136,7 +136,7 @@ def administrator():
     """
     import os
     import ctypes
-    return bool(ctypes.windll.shell32.IsUserAnAdmin() if os.name == 'nt' else os.getuid() == 0)
+    return bool(ctypes.WinDLL("shell32").IsUserAnAdmin() if 'nt' == os.name else os.getuid() == 0)
 
 
 def geolocation():
