12eb28cb4a8e166dfecebcc35da2de349fce91d2
ibex107 <91543573+ibex107@users.noreply.github.com>
2021-09-28 14:54:32 +0200

    Needs sudo permision to build docker images
    Without sudo the terminal returns permission denied error.

--- web-gui/startup.sh
+++ web-gui/startup.sh
@@ -52,9 +52,9 @@ python3 -m pip install -r requirements.txt
 # Build Docker images
 echo "Building Docker images - this will take a while, please be patient..."
 cd docker-pyinstaller
-docker build -f Dockerfile-py3-amd64 -t nix-amd64 .
-docker build -f Dockerfile-py3-i386 -t nix-i386 .
-docker build -f Dockerfile-py3-win32 -t win-x32 .
+sudo docker build -f Dockerfile-py3-amd64 -t nix-amd64 .
+sudo docker build -f Dockerfile-py3-i386 -t nix-i386 .
+sudo docker build -f Dockerfile-py3-win32 -t win-x32 .
 
 read -p "To use some Byob features, you must reboot your system. If this is not your first time running this script, please answer no. Reboot now? [Y/n]: " agreeTo
 #Reboots system if user answers Yes
